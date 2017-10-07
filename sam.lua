bbd9c22d792c31607eb0d2e1ee16ddccad5ed9af
Brian <asoracarbuncle@gmail.com>
2017-10-07 02:34:47 -0400

    Add initial file to project

---
+++ sam.lua
@@ -0,0 +1,183 @@
+function get_sets()
+
+	----------------------------------------------------------------------
+	-- Bind the keys you wish to use with GearSwap
+	----------------------------------------------------------------------
+	send_command('bind f9 gs c toggle idle set')
+
+
+	----------------------------------------------------------------------
+	-- Idle set
+	----------------------------------------------------------------------
+	-- Initializes an array to begin storing set data
+	sets.idle = {}
+
+	-- Idle : Default
+	sets.idle = {
+        ammo="Ginsen",
+        head="Flam. Zucchetto +1",
+        body="Flamma Korazin +1",
+        hands="Flam. Manopolas +1",
+        legs="Flamma Dirs +1",
+        feet="Flam. Gambieras +1",
+        neck="Loricate Torque",
+        waist="Grunfeld Rope",
+        left_ear="Odnowa Earring",
+        right_ear="Odnowa Earring +1",
+        left_ring="Defending Ring",
+        right_ring="Vocane Ring",
+        back="Reiki Cloak",
+	} -- end sets.idle
+
+
+	----------------------------------------------------------------------
+	-- Melee set
+	----------------------------------------------------------------------
+	-- Initialize an array to begin storing set data
+	sets.melee = {}
+
+	-- Melee : default
+	sets.melee = {
+        ammo="Ginsen",
+        head="Flam. Zucchetto +1",
+	    body="Kasuga Domaru +1",
+        hands="Flam. Manopolas +1",
+        legs="Wakido Haidate +1",
+        feet="Flam. Gambieras +1",
+        neck="Asperity Necklace",
+        waist="Ioskeha Belt",
+        left_ear="Brutal Earring",
+        right_ear="Cessance Earring",
+        left_ring="Petrov Ring",
+        right_ring="Rajas Ring",
+        back={ name="Takaha Mantle", augments={'STR+5','"Zanshin"+2','"Store TP"+2',}},
+	} -- end sets.melee
+
+
+	----------------------------------------------------------------------
+	-- Precast sets
+	----------------------------------------------------------------------
+	-- Initialize an array to begin storing set data
+	sets.precast = {}
+
+	-- Precast : Fast Cast
+	sets.precast.fastCast = {
+        ammo="Sapience Orb",
+        hands={ name="Leyline Gloves", augments={'Accuracy+15','Mag. Acc.+15','"Mag.Atk.Bns."+15','"Fast Cast"+3',}},
+        neck="Voltsurge Torque",
+        left_ear="Etiolation Earring",
+        right_ear="Loquac. Earring",
+        left_ring="Lebeche Ring",
+        right_ring="Prolix Ring",
+	} -- end sets.precast.fastCast
+
+
+	----------------------------------------------------------------------
+	-- Midcast sets
+	----------------------------------------------------------------------
+	-- Initialize an array to begin storing set data
+	sets.midcast = {}
+	sets.midcast.ws = {}
+
+	-- Midcast : Weapon Skill : Default
+	sets.midcast.ws.default = {
+        ammo="Ginsen",
+        head="Flam. Zucchetto +1",
+        body="Flamma Korazin +1",
+        hands="Flam. Manopolas +1",
+        legs="Flamma Dirs +1",
+        feet="Flam. Gambieras +1",
+        neck="Asperity Necklace",
+        waist="Grunfeld Rope",
+        left_ear="Brutal Earring",
+        right_ear="Cessance Earring",
+        left_ring="Petrov Ring",
+        right_ring="Ilabrat Ring",
+        back={ name="Takaha Mantle", augments={'STR+5','"Zanshin"+2','"Store TP"+2',}},
+	} -- end sets.midcast.ws.default
+
+	-- Midcast : Weapon Skill : Tachi: Fudo
+	sets.midcast.ws["Tachi: Fudo"] = sets.midcast.ws.default
+	-- end sets.midcast.ws["Tachi: Fudo"]
+
+	-- Midcast : Weapon Skill : Tachi: Shoha
+	sets.midcast.ws["Tachi: Shoha"] = sets.midcast.ws.default
+	-- end sets.midcast.ws["Tachi: Shoha"]
+
+
+end -- end get_sets()
+
+
+----------------------------------------------------------------------
+-- Callback for when casting begins
+----------------------------------------------------------------------
+function precast(spell)
+
+    if spell.action_type == 'Magic' then
+		equip(sets.precast.fastCast)
+	end
+
+end -- end precast()
+
+
+----------------------------------------------------------------------
+-- Callback for after casting begins, but before it fires
+----------------------------------------------------------------------
+function midcast(spell)
+
+	-- Check if the action is a specified weapon skill
+	if sets.midcast.ws[spell.name] then
+		-- Equip the appropriate specific ws set
+		equip(sets.midcast.ws[spell.name])
+    else
+    	-- Equip default ws set
+		equip(sets.midcast.ws.default)
+	end
+
+end -- end midcast()
+
+
+----------------------------------------------------------------------
+-- Callback for after casting has fired
+----------------------------------------------------------------------
+function aftercast(spell)
+
+	-- Check the player status
+	if player.status =='Engaged' then
+		equip(sets.melee)
+	else
+		equip(sets.idle)
+	end
+
+end -- end aftercast()
+
+
+----------------------------------------------------------------------
+-- Callback for whenever engagment status changes
+----------------------------------------------------------------------
+function status_change(new,old)
+
+	-- Check the player status
+	if new == 'Idle' then
+		equip(sets.idle)
+	elseif new == 'Engaged' then
+		equip(sets.melee)
+	end
+
+end -- end status_change()
+
+
+----------------------------------------------------------------------
+-- In game alerts to gear set changes
+----------------------------------------------------------------------
+function self_command(command)
+
+	-- Equip the idle set
+	if command == 'toggle idle set' then
+		-- Alert the user which set is currently being equipped
+		send_command('@input /echo <----- Idle Set Equipped ----->')
+		-- Equip the set
+		equip(sets.idle)
+	end -- end if
+
+end -- end self_command()
