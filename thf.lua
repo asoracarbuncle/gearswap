2f48fdef4aeef4314699c502452c392397c2838d
Brian <asoracarbuncle@gmail.com>
2018-03-02 07:12:44 -0500

    Rework gearsets and add another Ambuscade cape

--- thf.lua
+++ thf.lua
@@ -21,13 +21,13 @@ function get_sets()
 	    hands="Mummu Wrists +1",
 	    legs="Mummu Kecks +1",
 	    feet="Mummu Gamash. +1",
-        neck="Loricate Torque +1",
-        waist="Chaac Belt",
-        left_ear="Etiolation Earring",
-        right_ear="Static Earring",
-        left_ring="Defending Ring",
-        right_ring="Vocane Ring",
-        back={ name="Canny Cape", augments={'DEX+4','AGI+2','"Dual Wield"+1','Crit. hit damage +2%',}},
+	    neck="Loricate Torque +1",
+	    waist="Windbuffet Belt +1",
+	    left_ear="Etiolation Earring",
+	    right_ear="Static Earring",
+	    left_ring="Defending Ring",
+	    right_ring="Vocane Ring",
+	    back="Reiki Cloak",
 	} -- end sets.idle.default
 
 
@@ -38,21 +38,21 @@ function get_sets()
 	sets.melee = {}
 
 	-- Melee : default
-	sets.melee.default = {
-        ammo="Ginsen",
-        head={ name="Dampening Tam", augments={'DEX+10','Accuracy+15','Mag. Acc.+15','Quadruple Attack +3',}},
-        body={ name="Adhemar Jacket", augments={'DEX+10','AGI+10','Accuracy+15',}},
-        hands={ name="Adhemar Wristbands", augments={'DEX+10','AGI+10','Accuracy+15',}},
-        legs={ name="Samnuha Tights", augments={'STR+10','DEX+10','"Dbl.Atk."+3','"Triple Atk."+3',}},
-        feet={ name="Herculean Boots", augments={'Accuracy+24 Attack+24','"Triple Atk."+2','DEX+6','Accuracy+7','Attack+14',}},
-        neck="Asperity Necklace",
-        waist="Windbuffet Belt +1",
-        left_ear="Suppanomimi",
-        right_ear="Brutal Earring",
-        left_ring="Epona's Ring",
+	sets.melee = {
+	    ammo="Ginsen",
+	    head={ name="Dampening Tam", augments={'DEX+10','Accuracy+15','Mag. Acc.+15','Quadruple Attack +3',}},
+	    body={ name="Adhemar Jacket", augments={'DEX+10','AGI+10','Accuracy+15',}},
+	    hands={ name="Adhemar Wristbands", augments={'DEX+10','AGI+10','Accuracy+15',}},
+	    legs={ name="Samnuha Tights", augments={'STR+10','DEX+10','"Dbl.Atk."+3','"Triple Atk."+3',}},
+	    feet={ name="Herculean Boots", augments={'Accuracy+24 Attack+24','"Triple Atk."+2','DEX+6','Accuracy+7','Attack+14',}},
+	    neck="Clotharius Torque",
+	    waist="Windbuffet Belt +1",
+	    left_ear="Suppanomimi",
+	    right_ear="Brutal Earring",
+	    left_ring="Epona's Ring",
 	    right_ring="Hetairoi Ring",
-        back={ name="Toutatis's Cape", augments={'DEX+20','Accuracy+20 Attack+20','DEX+10','Crit.hit rate+10',}},
-	} -- end sets.melee.default
+	    back={ name="Toutatis's Cape", augments={'DEX+20','Accuracy+20 Attack+20','Accuracy+10','"Store TP"+10',}},
+	} -- end sets.melee
 
 
 	----------------------------------------------------------------------
@@ -64,41 +64,41 @@ function get_sets()
 
 	-- Midcast : Weapon Skill : Default
 	sets.midcast.ws.default = {
-	    ammo="Mantoptera Eye",
-	    head={ name="Dampening Tam", augments={'DEX+10','Accuracy+15','Mag. Acc.+15','Quadruple Attack +3',}},
-	    body={ name="Adhemar Jacket", augments={'DEX+10','AGI+10','Accuracy+15',}},
-	    hands={ name="Adhemar Wristbands", augments={'DEX+10','AGI+10','Accuracy+15',}},
-	    legs={ name="Samnuha Tights", augments={'STR+10','DEX+10','"Dbl.Atk."+3','"Triple Atk."+3',}},
-        feet={ name="Herculean Boots", augments={'Accuracy+24 Attack+24','"Triple Atk."+2','DEX+6','Accuracy+7','Attack+14',}},
-	    neck="Clotharius Torque",
-	    waist="Grunfeld Rope",
-	    left_ear="Telos Earring",
-	    right_ear={ name="Moonshade Earring", augments={'Accuracy+4','TP Bonus +25',}},
-	    left_ring="Ramuh Ring",
-	    right_ring="Begrudging Ring",
-	    back={ name="Toutatis's Cape", augments={'DEX+20','Accuracy+20 Attack+20','DEX+10','Crit.hit rate+10',}},
+        ammo="Falcon Eye",
+        head="Mummu Bonnet +1",
+        body="Mummu Jacket +1",
+        hands="Mummu Wrists +1",
+        legs="Mummu Kecks +1",
+        feet="Mummu Gamash. +1",
+        neck="Fotia Gorget",
+        waist="Fotia Belt",
+        left_ear="Telos Earring",
+        right_ear={ name="Moonshade Earring", augments={'Accuracy+4','TP Bonus +25',}},
+        left_ring="Regal Ring",
+        right_ring="Begrudging Ring",
+        back={ name="Toutatis's Cape", augments={'DEX+20','Accuracy+20 Attack+20','DEX+10','Crit.hit rate+10',}},
 	} -- end sets.midcast.ws.default
 
 	-- Midcast : Weapon Skill : Rudra's Storm
 	sets.midcast.ws["Rudra's Storm"] = {
-	    ammo="Mantoptera Eye",
-	    head="Mummu Bonnet +1",
-	    body="Mummu Jacket +1",
-	    hands="Mummu Wrists +1",
-	    legs="Mummu Kecks +1",
-	    feet="Mummu Gamash. +1",
-	    neck="Clotharius Torque",
-	    waist="Grunfeld Rope",
-	    left_ear="Telos Earring",
-	    right_ear={ name="Moonshade Earring", augments={'Accuracy+4','TP Bonus +25',}},
-	    left_ring="Ramuh Ring",
-	    right_ring="Begrudging Ring",
-	    back={ name="Toutatis's Cape", augments={'DEX+20','Accuracy+20 Attack+20','DEX+10','Crit.hit rate+10',}},
+        ammo="Falcon Eye",
+        head="Mummu Bonnet +1",
+        body="Mummu Jacket +1",
+        hands="Mummu Wrists +1",
+        legs="Mummu Kecks +1",
+        feet="Mummu Gamash. +1",
+        neck="Fotia Gorget",
+        waist="Fotia Belt",
+        left_ear="Telos Earring",
+        right_ear={ name="Moonshade Earring", augments={'Accuracy+4','TP Bonus +25',}},
+        left_ring="Regal Ring",
+        right_ring="Begrudging Ring",
+        back={ name="Toutatis's Cape", augments={'DEX+20','Accuracy+20 Attack+20','DEX+10','Crit.hit rate+10',}},
 	} -- end sets.midcast.ws["Rudra's Storm"]
 
 	-- Midcast : Weapon Skill : Evisceration
 	sets.midcast.ws['Evisceration'] = {
-	    ammo="Mantoptera Eye",
+        ammo="Falcon Eye",
 	    head={ name="Dampening Tam", augments={'DEX+10','Accuracy+15','Mag. Acc.+15','Quadruple Attack +3',}},
 	    body={ name="Adhemar Jacket", augments={'DEX+10','AGI+10','Accuracy+15',}},
 	    hands={ name="Adhemar Wristbands", augments={'DEX+10','AGI+10','Accuracy+15',}},
@@ -108,14 +108,14 @@ function get_sets()
 	    waist="Fotia Belt",
 	    left_ear="Telos Earring",
 	    right_ear={ name="Moonshade Earring", augments={'Accuracy+4','TP Bonus +25',}},
-	    left_ring="Ramuh Ring",
+        left_ring="Regal Ring",
 	    right_ring="Begrudging Ring",
 	    back={ name="Toutatis's Cape", augments={'DEX+20','Accuracy+20 Attack+20','DEX+10','Crit.hit rate+10',}},
 	} -- end sets.midcast.ws['Evisceration']
 
 	-- Midcast : Weapon Skill : Mandalic Stab
 	sets.midcast.ws['Mandalic Stab'] = {
-	    ammo="Mantoptera Eye",
+        ammo="Falcon Eye",
 	    head={ name="Dampening Tam", augments={'DEX+10','Accuracy+15','Mag. Acc.+15','Quadruple Attack +3',}},
 	    body={ name="Adhemar Jacket", augments={'DEX+10','AGI+10','Accuracy+15',}},
 	    hands={ name="Adhemar Wristbands", augments={'DEX+10','AGI+10','Accuracy+15',}},
@@ -125,7 +125,7 @@ function get_sets()
 	    waist="Fotia Belt",
 	    left_ear="Telos Earring",
 	    right_ear={ name="Moonshade Earring", augments={'Accuracy+4','TP Bonus +25',}},
-	    left_ring="Ramuh Ring",
+        left_ring="Regal Ring",
 	    right_ring="Begrudging Ring",
 	    back={ name="Toutatis's Cape", augments={'DEX+20','Accuracy+20 Attack+20','DEX+10','Crit.hit rate+10',}},
 	} -- end sets.midcast.ws['Evisceration']
@@ -153,13 +153,15 @@ end -- end get_sets()
 ----------------------------------------------------------------------
 function midcast(spell)
 
-	-- Check if the action is a specified weapon skill
-	if sets.midcast.ws[spell.name] then
-		-- Equip the appropriate ws specific set
-		equip(sets.midcast.ws[spell.name])
-    elseif spell.type == 'WeaponSkill' then
-    	-- Equip default ws set
-		equip(sets.midcast.ws.default)
+	-- Check if the action is a weapon skill
+	if spell.type == 'WeaponSkill' then
+		-- Equip specific weapon skill set
+		if sets.midcast.ws[spell.english] then
+			equip(sets.midcast.ws[spell.english])
+		-- Equip default weapon skill set
+		else
+			equip(sets.midcast.ws.default)
+		end
 	end
 
 end -- end midcast()
@@ -172,9 +174,9 @@ function aftercast(spell)
 
 	-- Check the player status
 	if player.status =='Engaged' then
-		equip(sets.melee.default)
+		equip(sets.melee)
 	else
-		equip(sets.melee.default)
+		equip(sets.melee)
 		equip(sets.idle.default)
 	end
 
@@ -195,7 +197,7 @@ function status_change(new,old)
 	if new == 'Idle' then
 		equip(sets.idle.default)
 	elseif new == 'Engaged' then
-		equip(sets.melee.default)
+		equip(sets.melee)
 	end
 
 	-- Check if treasure hunter is enabled
