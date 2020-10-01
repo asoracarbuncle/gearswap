0c97d7e8e1f456f25bdc5c692efc089988084a7a
Brian <asoracarbuncle@gmail.com>
2020-10-01 06:17:18 -0400

    Empty the gear sets

--- drk.lua
+++ drk.lua
@@ -10,19 +10,9 @@ function get_sets()
 	-- Idle set
 	----------------------------------------------------------------------
 	sets.idle = {
-        ammo="Hydrocera",
-        head="Hjarrandi Helm",
-        body="Hjarrandi Breast.",
-        hands="Flam. Manopolas +2",
-        legs="Flamma Dirs +2",
-        feet="Flam. Gambieras +2",
-        neck="Loricate Torque +1",
-        waist="Nierenschutz",
-        left_ear="Etiolation Earring",
-        right_ear="Static Earring",
-        left_ring="Fortified Ring",
-        right_ring="Defending Ring",
-        back="Moonbeam Cape",
+		-- PASTE GEAR FROM //gs export OVER THIS LINE
+		-- PASTE GEAR FROM //gs export OVER THIS LINE
+		-- PASTE GEAR FROM //gs export OVER THIS LINE
 	} -- end Idle
 
 
@@ -30,19 +20,9 @@ function get_sets()
 	-- Engaged set
 	----------------------------------------------------------------------
 	sets.engaged = {
-	    ammo="Ginsen",
-	    head="Flam. Zucchetto +2",
-	    body="Hjarrandi Breast.",
-	    hands="Flam. Manopolas +2",
-	    legs="Flamma Dirs +2",
-	    feet="Flam. Gambieras +2",
-	    neck="Ainia Collar",
-	    waist="Ioskeha Belt +1",
-	    left_ear="Telos Earring",
-	    right_ear="Dedition Earring",
-	    left_ring="Flamma Ring",
-	    right_ring="Rajas Ring",
-	    back="Atheling Mantle",
+		-- PASTE GEAR FROM //gs export OVER THIS LINE
+		-- PASTE GEAR FROM //gs export OVER THIS LINE
+		-- PASTE GEAR FROM //gs export OVER THIS LINE
 	} -- end Engaged
 
 
@@ -52,19 +32,9 @@ function get_sets()
 	----------------------------------------------------------------------
 	-- Precast : Fast Cast
 	sets.precast.fastCast = {
-        ammo="Impatiens",
-        head={ name="Carmine Mask", augments={'Accuracy+15','Mag. Acc.+10','"Fast Cast"+3',}},
-        body="Sacro Breastplate",
-        hands={ name="Leyline Gloves", augments={'Accuracy+15','Mag. Acc.+15','"Mag.Atk.Bns."+15','"Fast Cast"+3',}},
-        legs="Arjuna Breeches",
-        feet={ name="Carmine Greaves", augments={'HP+60','MP+60','Phys. dmg. taken -3',}},
-        neck="Orunmila's Torque",
-        waist="Nierenschutz",
-        left_ear="Etiolation Earring",
-        right_ear="Malignance Earring",
-        left_ring="Rahab Ring",
-        right_ring="Kishar Ring",
-        back="Moonbeam Cape",
+		-- PASTE GEAR FROM //gs export OVER THIS LINE
+		-- PASTE GEAR FROM //gs export OVER THIS LINE
+		-- PASTE GEAR FROM //gs export OVER THIS LINE
 	} -- end Fast Cast
 
 
@@ -77,19 +47,9 @@ function get_sets()
 	-- This set will be used to every weapon skill that does NOT
 	-- have a specific named set below.
 	sets.midcast.ws.default = {
-        ammo="Knobkierrie",
-        head="Flam. Zucchetto +2",
-        body="Hjarrandi Breast.",
-        hands="Flam. Manopolas +2",
-        legs="Flamma Dirs +2",
-        feet="Flam. Gambieras +2",
-        neck="Fotia Gorget",
-        waist="Fotia Belt",
-        left_ear="Ishvara Earring",
-        right_ear="Thrud Earring",
-        left_ring="Karieyh Ring",
-        right_ring="Flamma Ring",
-        back="Atheling Mantle",
+		-- PASTE GEAR FROM //gs export OVER THIS LINE
+		-- PASTE GEAR FROM //gs export OVER THIS LINE
+		-- PASTE GEAR FROM //gs export OVER THIS LINE
 	} -- end Weaponskill Default
 
 	-- Midcast : Weapon Skill : Resolution
@@ -253,13 +213,10 @@ function midcast(spell)
 	if spell.type == 'JobAbility' then
 		equip(sets.midcast.ja[spell.name])
     elseif spell.action_type == 'Magic' then
-		send_command('@input /echo Magic: General')
     	equip(sets.midcast.magic)
     	if DarkSpells[spell.english] then
-			send_command('@input /echo Magic: Dark')
 	    	equip(sets.utility.darkMagic)
     	elseif EnfeeblingSpells[spell.name] then
-			send_command('@input /echo Magic: Enfeebling')
 	    	equip(sets.utility.enfeeblingMagic)
     	end
     elseif spell.type == 'WeaponSkill' then
